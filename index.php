e7c115db372260192fcebe7b2c7e63b401661614
alexangria1992 <alex040892@gmail.com>
2020-08-07 12:32:41 +1200

    Fixed initial commit

--- index.php
+++ index.php
@@ -6,34 +6,50 @@
     <title>Online Library Management System</title>
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
+    <style type="text/css">
+
+    </style>
 <body>
     <div class="wrapper">  
         <header>
             <div class="logo">
-                <img src="images/9.png" alt="">
+                <img style="background-repeat:  no-repeat;;" src="images/9.png" alt="">
                 <h1 style="color: white">ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
             </div>
-            <nav>
+        <nav>
                 <ul>
                     <li><a href="">HOME</a></li>
+                    <li><a href="index.php">HOME</a></li>
                     <li><a href="">BOOKS</a></li>
-                    <li><a href="">STUDNET-LOGIN</a></li>
+                    <li><a href="student_login.php">STUDENT-LOGIN</a></li>
                     <li><a href="">ADMIN_LOGIN</a></li>
                     <li><a href="">FEEDBACK</a></li>
                 </ul>
             </nav>
         </header>
     <section>
-        <br><br><br>
+    <br><br><br>
             <div class="box">
                 <br><br><br><br>
                 <h1 style="text-align: center; font-size: 30px;">Welcome to library</h1><br>
                 <h1 style="text-align: center;font-size: 25px;">Opens at 09:00</h1><br>
                 <h1 style="text-align: center;font-size: 25px;">Closes at 15:00</h1><br>
             </div>
-    </section>
-    <footer>
-    </footer>
-</div>
+      
+        <section>
+       
+        </section>
+        <footer>
+        <p style="color: white; text-align: center;">
+            <br><br>
+              Email: &nbsb Online.library@gmail.com <br><br>
+              Mobile: &nbsp +880171****
+        </footer>
+    
+    </div>
+
+<script>
+     
+</script>
 </body>
 </html>  
